5b0a6199f4552b4cc3729119955ab4caf7031bc8
NoMoreNap <vv2trash@gmail.com>
2024-02-20 20:51:47 +0300

    lab 7

---
+++ task_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using  namespace  std;
+
+int pos = 3;
+
+int Fibonacci(int max) {
+    int sum = 0, prev = 0, current = 1;
+    while (true) {
+        sum = prev + current;
+        prev = current;
+        current = sum;
+        if (sum >= max ) {
+            break;
+        }
+        pos++;
+    }
+    return sum;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int max;
+
+    wcout << L"Введите максимальное число ( от 1 до 2^31 )\n";
+    cin >> max;
+
+    if (!cin.good() || max <= 1 || max > 2147483647-1) {
+        wcout << L"Введено неверное число";
+        return 0;
+    }
+
+    int findeded_int = Fibonacci( max);
+    wcout << L"Найденное число " << findeded_int << L" на позиции " << pos << L" в последовательности Фибоначчи";
+}
